e06e097956e2ae720a35d9c87a820ba51672f9d1
Brian huang <237890675+brianhuang1998-art@users.noreply.github.com>
2026-09-22 00:12:30 +0800

    損益兩平/賣出價標籤加上單位與說明

--- calc.js
+++ calc.js
@@ -94,7 +94,7 @@ function updateCalculator(){
   const mBe = $('markerBe'), lBe = $('labelBe'), mSell = $('markerSell'), lSell = $('labelSell');
   mBe.style.left = bePct + '%';
   lBe.style.left = bePct + '%';
-  lBe.innerHTML = '損益兩平 ' + sel.breakeven.toFixed(2) + ' 元<span class="lbl-note">超過才賺錢，低於就賠錢</span>';
+  lBe.innerHTML = '損益兩平 ' + sel.breakeven.toFixed(2) + ' 元<span class="lbl-note">（超過才賺錢，低於就賠錢。）</span>';
 
   mSell.style.left = sellPct + '%';
   lSell.style.left = sellPct + '%';
